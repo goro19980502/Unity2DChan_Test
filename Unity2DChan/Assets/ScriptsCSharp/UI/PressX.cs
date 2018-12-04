b8cf6e831baad27c544844372af0c55058d3dc01
goro19980502 <goro19980502@gmail.com>
2018-12-04 15:45:04 +0800

    tutorial_Ok

---
+++ Unity2DChan/Assets/ScriptsCSharp/UI/PressX.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI ;
+using System.Collections;
+
+public class PressX : MonoBehaviour {
+
+	void Start () {
+		InvokeRepeating("ShowText", 0, 0.5f) ;
+	}
+	
+	void ShowText () {
+		if (Input.GetKey("x"))
+		{
+			Application.LoadLevel(2);
+		}
+
+	}
+}
